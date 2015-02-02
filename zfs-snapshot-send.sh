d60e7de6bf7611972e8a868d30ae329f800ccba7
James Ryan Cresawn <jrcresawn@gmail.com>
2015-02-02 16:15:50 -0700

    Adds logic to prevent zfs scrub and zfs send from running at the same time.

--- zfs-snapshot-send.sh
+++ zfs-snapshot-send.sh
@@ -3,32 +3,36 @@
 # usage: zfs-snapshot-send.sh [ZFS filesystem]
 # example: zfs-snapshot-send.sh
 # example crontab entry
-# 30 0 * * 6 /opt/sbin/zfs-snapshot-send.sh
+# 00 17 * * 5 /opt/sbin/zfs-snapshot-send.sh
 
 # This expression returns 0 if the week of the year is divisible by 4.
-scrub=$(expr `date +%U` % 4)
+scrubweek=$(expr `date +%U` % 4)
+scrubinprogress=$(zpool status | grep -q 'scan: scrub in progress')$?
+sendinprogress=$(test -d /var/run/zfs-snapshot-send.lock)$?
 
-if [ $scrub == 0 ]; then
-    # The current week is divisible by 4; therefore, initiate scrub instead of send.
-    zpool scrub rpool1 cals
-    exit 0
-else
-    if mkdir /var/run/zfs-snapshot-send.lock; then
-	echo "Lock succeeded" > /var/log/zfs-snapshot-send.log
-	
-	# manage backup snapshots
-	/opt/sbin/zfs-snapshot.sh rpool1 backup 2
-	/opt/sbin/zfs-snapshot.sh cals backup 2
+if [ $scrubinprogress != 0 ] && [ $sendinprogress != 0 ]; then
+    if [ $scrubweek == 0 ]; then  # initiate scrub
+	zpool scrub rpool1 cals
+    else  # initiate send
+	if mkdir /var/run/zfs-snapshot-send.lock; then
+	    echo "Lock succeeded" > /var/log/zfs-snapshot-send.log
+	    
+            # manage backup snapshots
+	    /opt/sbin/zfs-snapshot.sh rpool1 backup 2
+	    /opt/sbin/zfs-snapshot.sh cals backup 2
 
-        # identify ZFS filesystem snapshots and send them to the destination
-	for i in `zfs list -H -t filesystem -o name $@` ; do
-	    dest=`echo $i | sed 's/\//_/g'`
-	    nice -n 19 zfs send $i@backup.0 > /backup/$dest
-	done
+            # identify ZFS filesystem snapshots and send them to the destination
+	    for i in `zfs list -H -t filesystem -o name $@` ; do
+		dest=`echo $i | sed 's/\//_/g'`
+		nice -n 19 zfs send $i@backup.0 > /backup/$dest
+	    done
 	
-	rmdir /var/run/zfs-snapshot-send.lock
-    else
-	echo "Lock failed - exit"
-	exit 1
+	    rmdir /var/run/zfs-snapshot-send.lock
+	else
+	    echo "Lock failed - exit"
+	    exit 1
+	fi
     fi
+else
+    echo 'The zpools are busy with other I/O operations this week.'
 fi
